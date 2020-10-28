52050f4eec6ee78a8c23144fec476e5ab952b9bb
Wenn Kuan <kuanwennsin@live.com>
2020-10-28 17:20:07 +0800

    delete

--- cake-scripts/list.js
+++ cake-scripts/list.js
@@ -1,4 +1,4 @@
-window.addEventListener("load",function(){
+window.addEventListener("load", function () {
     GetBookings();
 });
 
@@ -17,6 +17,7 @@ window.addEventListener("load",function(){
             var gEmail = json.bookings[i].email;
             var gPax = json.bookings[i].pax;
             var gId = json.bookings[i].id; //is the row id from the google sheet
+            var gRemarks = json.bookings[i].remarks;
             var buttonId = "delete" + gId; //this is for the delete button next to the booking IDs
 
             let row = bookings.insertRow(bookings.rows.length);
@@ -24,7 +25,7 @@ window.addEventListener("load",function(){
             row.insertCell(1).innerHTML = gName;
             row.insertCell(2).innerHTML = gEmail;
             row.insertCell(3).innerHTML = gPax;
-            row.insertCell(4).innerHTML = ""; //remarks
+            row.insertCell(4).innerHTML = gRemarks; //remarks
             row.insertCell(5).innerHTML = "<button id='" + buttonId + "' class='btn btn-danger'>Delete</button><br/>";
 
             bookingIds.push(buttonId);
@@ -35,7 +36,7 @@ window.addEventListener("load",function(){
             let el = document.getElementById(bookingIds[j]);
             el.addEventListener("click",function(){
             let theId = bookingIds[j].replace("delete","");
-            console.log(theId);
+            DeleteBooking(theId);
             });
         }
 
