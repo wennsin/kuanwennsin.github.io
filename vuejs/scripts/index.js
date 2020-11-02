574bfb3dc03aa97939bc7b5200658426abd1770d
Wenn Kuan <kuanwennsin@live.com>
2020-11-02 20:11:03 +0800

    added linkurl and linktext

--- vuejs/scripts/index.js
+++ vuejs/scripts/index.js
@@ -9,5 +9,7 @@ var app2 = new Vue ({
     el : "#app-2",
     data : {
         message : "You loaded this page on " + new Date ().toLocaleString()
+        linkURL : "https://www.google.com.my",
+        linkText : "Go to Google Malaysia!"
     }
 });
